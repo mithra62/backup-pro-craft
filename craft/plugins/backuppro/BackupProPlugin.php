f6a67dca6171603ea506c22360d1e625165fc118
Eric Lamb <eric@mithra62.com>
2016-04-09 10:54:40 -0700

    Adds check for requirements before installation can happen

--- craft/plugins/backuppro/BackupProPlugin.php
+++ craft/plugins/backuppro/BackupProPlugin.php
@@ -89,6 +89,25 @@ class BackupProPlugin extends BasePlugin implements BackupPro
         return true;
     }
     
+    public function onBeforeInstall()
+    {
+        if (version_compare(phpversion(), '5.4.0', '<')) {
+            throw new \Exception('You must be run PHP 5.4 or greater to use this package.');
+        }
+        
+        if (!function_exists('mysqli_report') && !class_exists('PDO')) {
+            throw new \Exception('Backup Pro requires either the mysqli or PDO extension for database use.');
+        }
+        
+        if (!function_exists('mb_check_encoding')) {
+            throw new \Exception('Backup Pro requires the Multi Byte extension to ensure proper string encoding.');
+        }
+        
+        if (!function_exists('curl_init')) {
+            throw new \Exception('Backup Pro requires the Curl extension to transfer backups to remote locations.');
+        }
+    }
+    
     /**
      * (non-PHPdoc)
      * @see \Craft\BasePlugin::onBeforeUninstall()
